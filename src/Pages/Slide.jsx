e01c95d11aab3a8ecbaf303e5225b140ec21ddf2
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-13 17:03:15 +0530

    banner complete

--- src/Pages/Slide.jsx
+++ src/Pages/Slide.jsx
@@ -4,7 +4,7 @@ import { ReactTyped } from 'react-typed';
 const Slide = () => {
     const slideStyle = {
         backgroundImage: "url('/assets/slide.jpg')",
-        height: '80vh',
+        // height: '50vh',
         width: '100vw',
         backgroundPosition: 'center top',
         backgroundSize: 'cover',
@@ -16,8 +16,8 @@ const Slide = () => {
     };
 
     return (
-        <div className='slide' style={slideStyle}>
-            <h1 className='text-[40px] font-bold pt-10 pr-5 text-center'>
+        <div className='slide h-[50vh] md:h-[80vh]' style={slideStyle}>
+            <h1 className='md:text-[40px] text-[30px] font-bold pt-10 md:pr-5 text-center'>
                 Welcome Home! Anywhere you roam <br /> 
                 <ReactTyped className='pl-3' strings={['Stay in the moment.', 'Make your memories.']} typeSpeed={100} backSpeed={60} loop/>
              
