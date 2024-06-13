3b4ba00400a23e53ef010a0c6b9e41aced2928e9
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-13 16:59:01 +0530

    header complete

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,27 +14,27 @@ const Navbar = () => {
             alert(search)
         }
   return (
-    <div className='bg-white shadow-xl'>
-        <div className=' navbar flex py-[10px]  px-4 sm:px-6 lg:px-8 justify-between items-center relative'>
+    <div className='mx-auto '>
+        <div className='navbar flex md:py-[10px] md:px-[60px] justify-between items-center relative shadow-2xl'>
             <Link to='/'>
-            <img src='/assets/logo.png' alt='Trip-go' className='md:w-[100px] w-[95px] h-full cursor-pointer'/>
+            <img src='/assets/logo.png' alt='Trip-go' className='w-[100px] h-full cursor-pointer'/>
 
             </Link>
-            <div className='navbar_search border border-gray-400 rounded-[30px] md:h-[50px] h-[30px] flex items-center gap-4 md:gap-10 p-2 md:p-4'>
+            <div className='navbar_search border border-gray-400 rounded-[30px] h-[50px] flex items-center md:w-1/4 gap-10  p-4'>
                 <input type='text' placeholder='Search...' value={search} onChange={(e)=>setSearch(e.target.value)} className='outline-none w-full border-none'/>
                 <CiSearch className='h-[70px] w-[30px] cursor-pointer' onClick={alert2} />
             </div>
-            <div className='navbar_right flex ml-4 md:w-1/3 items-center gap-5 sm:text-[10px]'>
-                <Link className=''>
+            <div className='navbar_right flex items-center gap-5'>
+                <Link className='ml-2'>
                 Become A Host
                 </Link>
-                <button className='navbar_right_button h-[40px] md:h-[50px] flex items-center p-4 m-1 border border-gray-500 rounded-[30px] gap-2 bg-white cursor-pointer' onClick={(e)=>setDropdownMenu(!dropdownMenu)}>
-                    <CiMenuFries className='mr-2'/>
+                <button className='navbar_right_button md:h-[50px] flex items-center p-3 md:p-4 m-1 border border-gray-500 rounded-[30px] gap-2 bg-white cursor-pointer' onClick={(e)=>setDropdownMenu(!dropdownMenu)}>
+                    <CiMenuFries className='mr-2 '/>
                     <IoPerson/>
                 </button>
                 {
                     dropdownMenu &&(
-                        <div className='navbar_right_accountmenu absolute bg-white right-[60px] top-20 flex flex-col w-[150px] pt-2 border-gray-300 rounded-2xl items-center shadow-lg p-2 z-auto text-center'>
+                        <div className='navbar_right_accountmenu absolute bg-white md:right-[60px] right-[10px] md:top-20 top-[50px] flex flex-col w-[100px] md:w-[150px] pt-2 border-gray-300 rounded-2xl items-center shadow-lg p-2 z-auto text-center'>
                             <Link to="/login" className='text-[18px] rounded-2xl font-semibold w-full   hover:bg-slate-100'>Log In</Link>
                             <Link to="/register" className='text-[18px]  rounded-2xl font-semibold w-full   hover:bg-slate-100'>Sign Up</Link>
                         </div>
@@ -46,4 +46,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
