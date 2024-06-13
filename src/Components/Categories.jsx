00c7eb5d02c98436cfd85e80667438cdf86cc79a
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-13 17:33:30 +0530

    category added

---
+++ src/Components/Categories.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div className='Categories py-[50px] px-[60px]  bg-gray-100'>
+        <div className='flex flex-col items-center text-center '>
+ <h1 className='text-[50px] text-blue-950 font-extrabold mb-4'>Explore Top Categories</h1>
+        <p className='max-w-[700px] text-[20px]'>
+        Explore our wide range of vacation rentals that cater to all types of
+        travelers. Immerse yourself in the local culture, enjoy the comforts of
+        home, and create unforgettable memories in your dream destination.
+        </p>
+        </div>
+       
+      
+    </div>
+  )
+}
+
+export default Categories
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Slide from './Slide'
+import Categories from '../Components/Categories'
 const Home = () => {
   return (
     <>
      <Navbar/>
      <Slide/>
+     <Categories/>
     </>
        
    
