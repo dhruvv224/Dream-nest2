c7dacd89d61623c6c13c45601d00339902cd652f
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-17 18:57:20 +0530

    unfiexed loader

--- src/Pages/Listing.jsx
+++ src/Pages/Listing.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { categories } from '../Data';
+import Loader from '../Components/Loader';
 
 const Listing = () => {
     const [selectedCategory, setSelectedCategory] = useState('ALL');
     console.log(categories);
+    const[isloading,setisLoading]=useState(true)
 
     const categoryContainerStyle = {
         padding: '50px 60px',
@@ -25,8 +27,11 @@ const handleCategory=(category)=>{
 }
     return (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
+                       
+
             <div className='max-w-[1200px]' style={{ width: '100%', maxWidth: '1200px', margin: '0 auto' }}>
                 <div style={categoryContainerStyle}>
+              
                     {categories.map((item, index) => (
                         <div className={`category1 ${selectedCategory === item.label ? 'text-red-600' : 'text-black'} hover:text-red-600 duration-200 text-black text-[30px]`} style={categoryItemStyle} key={index} onClick={()=>handleCategory(item.label)}>
                         <div className='category_icons ' >
@@ -39,6 +44,13 @@ const handleCategory=(category)=>{
                     ))}
                 </div>
             </div>
+            <div className='flex items-center justify-center '>
+            {
+                        isloading &&
+                        <Loader/>
+                    }
+            </div>
+                  
         </div>
     );
 };
