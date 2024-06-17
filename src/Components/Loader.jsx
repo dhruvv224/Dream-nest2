29e77145728dc36b6bff7369eae14886646bc67c
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-17 18:32:29 +0530

    added mui

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <CircularProgress />
+    </Box>
+  );
+};
 
-export default Loader
+export default Loader;
