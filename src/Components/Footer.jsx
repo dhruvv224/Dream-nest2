780ae8a3d9da2b5a693e2af7f43b02c9e75191f1
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-17 17:22:02 +0530

    footer completed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { IoMdCall } from "react-icons/io";
+import { CiMail } from "react-icons/ci";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-        <div className='footer_left max-w-[400px]'>
-            <Link to='/'><img src='/assets/logo.png' alt='logo'/>
+    <div className='footer grid grid-cols-3 gap-[50px] pt-2 pr-14 '>
+        <div className='footer_left max-w-[400px] ml-7'>
+            <Link to='/'><img src='/assets/logo.png' alt='logo' className='max-w-[150px] mb-6'/>
             </Link>
+            
+        </div>
+        <div className='footer_center'>
+        <h3 className='text-[20px] font-semibold mb-3'>Useful Links</h3>
+        <ul className='text-[16px] font-normal text-gray-600 '>
+          <li className='hover:text-red-500 duration-200 cursor-pointer'>About Us</li>
+          <li className='hover:text-red-500 duration-200 cursor-pointer'>Terms and Conditions</li>
+          <li className='hover:text-red-500 duration-200 cursor-pointer'>Return and Refund Policy</li>
+        </ul>
+
+        </div>
+        <div className='footer_right'>
+        <h2 className='text-[20px] font-semibold mb-3'>Contact</h2>
+            <div className='flex items-center mb-2 text-[16px]'>
+          
+            <IoMdCall/>
+            <p className='ml-[10px]'>+1 234 567 890</p>
+            </div>
+         <div className='flex items-center mb-2 text-[16px]'>
+ 
+         <CiMail/>
+            <p className='ml-[10px]'>dreamnest@support.com</p>
+         </div>
+          <img src='/assets/payment.png' className=''/>
+
             
         </div>
 
