487fe3ddffdb87d3b6ef4b122329ba0a3ef894c2
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-21 14:51:43 +0530

    fix

--- src/Pages/Createlistings.jsx
+++ src/Pages/Createlistings.jsx
@@ -90,39 +90,39 @@ const Createlistings = () => {
 
  }
 //   creator id
-const creatorId=useSelector((state)=>state.user.user.firstName)
+// const creatorId=useSelector((state)=>state.user.user.firstName)
 // console.log("creator id is",creatorId)
     const handlePost = async(e) => {
          
         e.preventDefault();  
-try {
-    const listingForm=new FormData()
-    listingForm.append("creator",creatorId)
-    listingForm.append("category",category)
-    listingForm.append("type",type);
-    listingForm.append("streetAddress",formLocation.streetAddress)
-    listingForm.append("aptSuite",formLocation.aptSuite)
-    listingForm.append("city",formLocation.city)
-    listingForm.append("province",formLocation.province)
-    listingForm.append("country",formLocation.country)
-    listingForm.append("guestCount",guestCount)
-    listingForm.append("bedroomCount",bedroomCount)
-    listingForm.append("bedCount",bedCount);
-    listingForm.append("bathroomCount",bathroomCount);
-    listingForm.append("amenities",amenities);
-    listingForm.append("title",formDescription.title)
-    listingForm.append("description",formDescription.description)
-    listingForm.append("highlight",formDescription.highlight)
-    listingForm.append("highlightDesc",formDescription.highlightDeec);
-    listingForm.append("price",formDescription.price)
-    const response=await axios.post("http://localhost:8000/api/listings/createlistings",listingForm)
-    console.log(response)
-    console.log(listingForm)
-} catch (error) {
-    console.log("there is an error",error.message)
+// try {
+//     const listingForm=new FormData()
+//     listingForm.append("creator",creatorId)
+//     listingForm.append("category",category)
+//     listingForm.append("type",type);
+//     listingForm.append("streetAddress",formLocation.streetAddress)
+//     listingForm.append("aptSuite",formLocation.aptSuite)
+//     listingForm.append("city",formLocation.city)
+//     listingForm.append("province",formLocation.province)
+//     listingForm.append("country",formLocation.country)
+//     listingForm.append("guestCount",guestCount)
+//     listingForm.append("bedroomCount",bedroomCount)
+//     listingForm.append("bedCount",bedCount);
+//     listingForm.append("bathroomCount",bathroomCount);
+//     listingForm.append("amenities",amenities);
+//     listingForm.append("title",formDescription.title)
+//     listingForm.append("description",formDescription.description)
+//     listingForm.append("highlight",formDescription.highlight)
+//     listingForm.append("highlightDesc",formDescription.highlightDeec);
+//     listingForm.append("price",formDescription.price)
+//     const response=await axios.post("http://localhost:8000/api/listings/createlistings",listingForm)
+//     console.log(response)
+//     console.log(listingForm)
+// } catch (error) {
+//     console.log("there is an error",error.message)
     
     
-}
+// }
     };
 
     return (
