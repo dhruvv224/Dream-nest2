8cc57a7b75bd96fe466fc0b84aae9097778ca065
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-24 19:32:49 +0530

    fix

--- src/Pages/Listing.jsx
+++ src/Pages/Listing.jsx
@@ -4,11 +4,23 @@ import Loader from '../Components/Loader';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { setListings } from '../Store/Slice';
+import { BiArrowBack } from 'react-icons/bi';
+import {
+    ArrowForwardIos,
+    ArrowBackIosNew,
+    Favorite,
+  } from "@mui/icons-material";
 const Listing = () => {
     const [selectedCategory, setSelectedCategory] = useState('All');
     const [isLoading, setIsLoading] = useState(true);
     const [categoryNotFound, setCategoryNotFound] = useState(false);
-
+    const[currentIndex,setCurrentIndex]=useState(0);
+    const goToPrevSlide=()=>{
+        setCurrentIndex((prevIndex)=>(prevIndex-1+listings.listingPhotoPaths.length)%listings.listingPhotoPaths.length)
+    }
+    const goToNextSlide=()=>{
+        setCurrentIndex((prevIndex)=>(prevIndex+1)%listings.listingPhotoPaths.length)
+    }
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -79,21 +91,40 @@ const Listing = () => {
                         </div>
                     ))}
                     <div>
-                        {isLoading ? <Loader /> :
-                            listings.map((item, index) => (
-                                <div className='' key={index}>
-                                    <div className='photo-galary'>
-                                        {
-                                            item.listingPhotoPaths.map((photoPath,photoIndex)=>(
-                                                <div className='photo-galary'>
-                                        <img src={`http://localhost:8000/${photoPath}`} className='rounded-[15px]' alt={`${item.creator}`}/>
+                    {isLoading ? <Loader /> :
+                            listings.map((item, index) => {
+                                const [currentIndex, setCurrentIndex] = useState(0);
 
-                                                </div>
-                                            ))
-                                        }
+                                const goToPrevSlide = () => {
+                                    setCurrentIndex((prevIndex) => (prevIndex - 1 + item.listingPhotoPaths.length) % item.listingPhotoPaths.length);
+                                }
+
+                                const goToNextSlide = () => {
+                                    setCurrentIndex((prevIndex) => (prevIndex + 1) % item.listingPhotoPaths.length);
+                                }
+
+                                return (
+                                    <div className='relative' key={index}>
+                                        <div className='slide-container overflow-hidden'>
+                                            <div className='slider flex transition-transform duration-500 ease-in-out' style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+                                                {
+                                                    item.listingPhotoPaths.map((photoPath, photoIndex) => (
+                                                        <div className='min-w-full h-full' key={photoIndex}>
+                                                            <img src={`http://localhost:8000/${photoPath}`} className='w-full h-full object-cover rounded-[15px]' alt={`${item.creator}`} />
+                                                        </div>
+                                                    ))
+                                                }
+                                            </div>
+                                            <button onClick={goToPrevSlide} className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black text-white p-2">
+                                                <ArrowBackIosNew />
+                                            </button>
+                                            <button onClick={goToNextSlide} className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black text-white p-2">
+                                                <ArrowForwardIos />
+                                            </button>
+                                        </div>
                                     </div>
-                                </div>
-                            ))
+                                );
+                            })
                         }
                     </div>
                     {categoryNotFound && <div>No listings found for selected category.</div>}
