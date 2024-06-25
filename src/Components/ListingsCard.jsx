a19d3d19fec99797dfc70f1ae0f1f5ca985aca32
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-25 18:32:18 +0530

    fetch

--- src/Components/ListingsCard.jsx
+++ src/Components/ListingsCard.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
-
+import axios from 'axios'
 const ListingsCard = () => {
     const id=useParams()
     console.log(id)
-    const listings=useSelector((state)=>state.listings)
-    const listingById=listings.filter(item=>item._id===id)
-    console.log(listingById)
+    // const listings=useSelector((state)=>state.listings)
+    // const listingById=listings.filter(item=>item._id===id)
+    // console.log(listingById)
+    const fetchData=async()=>{
+try {
+  const response=await axios.get(`http://localhost:8000/api/listings/id/${id}`)
+  console.log(response)
+  
+} catch (error) {
+  
+}
+
+    }
+    useEffect(()=>{
+      fetchData()
+    },[])
   return (
     <div>ListingsCard</div>
   )
