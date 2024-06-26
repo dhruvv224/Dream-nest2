c4f3fbbe635ba9df4b73a0926cc7bc485b3e2ecc
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-26 16:31:38 +0530

    d

--- src/Components/ListingsCard.jsx
+++ src/Components/ListingsCard.jsx
@@ -29,24 +29,36 @@ const ListingsCard = () => {
       <div className=''>
 
       <Navbar/>
-        <div className='listing-details md:pt-[40px] md:pr-[130px] md:pb-[120px] pt-[40px] pr-[20px] pb-[120px] max-w-[1200px] mx-auto'>
+        <div className='listing-details p-12 mt-[20px] pt-[40px] mx-auto'>
             {listing ? (
               
                 <div>
                   <div className='title flex justify-between items-center sm:flex-col sm:items-start sm:gap-[15px]'>
                   <h1 className=' text-[36px] font-semibold'>{listing.title}</h1>
                   </div>
-                  <div className="photos flex flex-wrap gap-3 mt-[12px]">
+                  <div className="photos flex flex-wrap  gap-3 mt-[12px]">
                     {
                         listing.listingPhotoPaths.map((item)=>(
-                            <img src={`http://localhost:8000/${item}`} className='max-w-[280px]'/>
+                            <img src={`http://localhost:8000/${item}`} className='max-w-[290px] '/>
                         ))
                     }
 
 
                     </div>
+                    <h2 className='text-[20px] font-semibold mt-4'>
+                        {listing.type} in {listing.city},{listing.province},{" "}{listing.country}
+                    </h2>
+                    <p className='text-[18px] font-normal mt-4'>
+                    {listing.guestCount} guests - {listing.bedroomCount} bedroom(s) -{" "}
+                    {listing.bedCount} bed(s) - {listing.bathroomCount} bathroom(s)
+                    </p>
+                    <hr/>
+                    <div className='profile'>
+                        <h3 className='text-[18px] font-normal'>Hosted by {listing.creator}</h3>
+
+                        </div>
                   
-                    <p>{listing.description}</p>
+                    {/* <p>{listing.description}</p>
                     <p><strong>Category:</strong> {listing.category}</p>
                     <p><strong>Type:</strong> {listing.type}</p>
                     <p><strong>Address:</strong> {listing.streetAddress}, {listing.city}, {listing.province}, {listing.country}</p>
@@ -56,7 +68,7 @@ const ListingsCard = () => {
                     <p><strong>Beds:</strong> {listing.bedCount}</p>
                     <p><strong>Bathrooms:</strong> {listing.bathroomCount}</p>
                     <p><strong>Price:</strong> ${listing.price}</p>
-                    <p><strong>Amenities:</strong> {listing.amenities.join(', ')}</p>
+                    <p><strong>Amenities:</strong> {listing.amenities.join(', ')}</p> */}
                     <div>
                         <strong>Photos:</strong>
                         {listing.listingPhotoPaths.map((path, index) => (
