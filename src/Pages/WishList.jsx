740cc09074d9d4d6c6e231d39d0d66b58cd22232
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-27 15:20:13 +0530

    almost done

--- src/Pages/WishList.jsx
+++ src/Pages/WishList.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import Navbar from '../Components/Navbar'
+import { Favorite } from '@mui/icons-material'
+import { setListings } from '../Store/Slice'
+import { useDispatch } from 'react-redux'
+import { setWishList } from '../Store/Slice'
+import { Link } from 'react-router-dom'
 const WishList = () => {
+    const dispatch=useDispatch();
 const Wishlist=useSelector((state)=>state.user.Wishlist)
+const listings=useSelector((state)=>state.user.listings);
+
 console.log(Wishlist)
+const toggleWishlist=(index)=>{
+    
+    const updatedListings=[...listings];
+    updatedListings[index]={...updatedListings[index],liked: !updatedListings[index].liked}
+    dispatch(setListings({listings:updatedListings}))
+    const likedItems=updatedListings.filter(item=>item.liked)
+    console.log("liked items >>>>>>",likedItems)
+    dispatch(setWishList({Wishlist:likedItems}))
+
+}
   return (
-    <div>
-        <Navbar/>
+    <>
+   
+    <Navbar/>
+    <div className='p-5'>
+       
         <h1 className='title list'> Your Wish Lists are</h1>
         <div className='list grid grid-cols-2 md:grid-cols-4 gap-4'>
             {
@@ -18,12 +39,37 @@ console.log(Wishlist)
                                 item.listingPhotoPaths.map((photoPath,index)=>(
                                     <div className=' w-[300px] h-[270px] flex-shrink-0' key={index}>
                                       <img src={`http://localhost:8000/${photoPath}`} className='w-full h-full object-cover' alt={`${item.creator}`} />
+                                      {
+                                                            !item.liked ? (
+                                                                <Favorite className={`absolute top-3 right-3 text-white hover:text-gray-100 duration-100 ${item.liked ? 'text-red-600' : 'text-white'}`} onClick={() => toggleWishlist(index)} />
 
+                                                            ):(
+                                                                <Favorite className={`absolute top-3 right-3 text-red-500 hover:text-red-600 duration-100`} onClick={() => toggleWishlist(index)} />
+
+                                                            )
+                                                        }
                                   </div>
                                 ))
                             }
                         </div>
                           </div>
+                          <div className='flex flex-col'>
+                            <Link to={`/listings/${item._id}`}>
+                            <h1 className='text-[16px] font-semibold'>
+                                                {item.title}
+                                            </h1>
+                                            <h4 className='text-[15px]'>{item.category}</h4>
+                                            <h3 className='text-[15px]'>{item.type}</h3>
+                                            <div className='flex items-center'>
+                                                <h5 className='text-[15px] font-medium'>₹{item.price}</h5>
+                                                {item.category === "Iconic cities" ? (
+                                                    <span className='inline ml-1'>Per Month</span>
+                                                ) : (
+                                                    <span className='inline ml-1'>Per Night</span>
+                                                )}
+                                            </div>
+                            </Link>
+                            </div>
 
                     </div>
                 ))
@@ -32,6 +78,7 @@ console.log(Wishlist)
 
         </div>
     </div>
+    </>
   )
 }
 
